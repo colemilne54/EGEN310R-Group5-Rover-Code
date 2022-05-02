d7912bfa39e391cf7c27105711efde5b4a5dcddd
Cole Milne <colemilne54@gmail.com>
2022-05-01 20:33:37 -0600

    Update rover-code.py

--- rover-code.py
+++ rover-code.py
@@ -1,20 +1,11 @@
-print("before imports")
- 
 import curses
 from pololu_drv8835_rpi import motors, MAX_SPEED
 import time
  
-print("before screen")
- 
 screen = curses.initscr()
-print("1")
 curses.noecho()
-print("2")
 curses.cbreak()
-print("3")
 screen.keypad(True)
- 
-print("before defs")
  
 def forwards(spd):
 	motors.setSpeeds(-spd, -spd)
@@ -33,40 +24,38 @@ def stop():
  
 def sleepOne():
 	time.sleep(1)
- 
-print("before try")
- 
- 
- 
+
+
 try:
 	while True:
-		print("before char")
 		char = screen.getch()
-		#char = input()
 		print("after char")
 		if char == ord('p'):
-		#if char == 'p':
 			break
 		elif char == ord('w'):
-		#elif char == 'w':
 			forwards(200)
 			sleepOne()
 			stop()
 		elif char == ord('s'):
-		#elif char == 's':
 			backwards(200)
 			sleepOne()
 			stop()
 		elif char == ord('a'):
-		#elif char == 'a':
-			left(400)
+			left(100)
 			sleepOne()
 			stop()			
 		elif char == ord('d'):
-		#elif char == 'd':
-			right(400)
+			right(100)
 			sleepOne()
-			stop()	
+			stop()
+		elif char == ord('i'):
+			forwards(300)
+			sleepOne()
+			stop()
+		elif char == ord('k'):
+			backwards(300)
+			sleepOne()
+			stop()
  
  
 finally:
